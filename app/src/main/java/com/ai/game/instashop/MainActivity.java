2f446c92faffa33bf750c514a1f6d274e91b10ef
aniketj2002 <aniketjindal2002@gmail.com>
2022-04-14 19:22:14 +0530

    testing

--- app/src/main/java/com/ai/game/instashop/MainActivity.java
+++ app/src/main/java/com/ai/game/instashop/MainActivity.java
@@ -3,12 +3,15 @@ package com.ai.game.instashop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textbox);
     }
 }
