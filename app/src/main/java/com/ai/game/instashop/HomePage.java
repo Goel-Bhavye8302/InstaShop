7a4aa904f4c08acd0f5fb9560e91a62b1bf8d8da
Bhavye Goel <76481947+Goel-Bhavye8302@users.noreply.github.com>
2022-06-26 16:47:25 +0530

    Update HomePage.java

--- app/src/main/java/com/ai/game/instashop/HomePage.java
+++ app/src/main/java/com/ai/game/instashop/HomePage.java
@@ -69,6 +69,7 @@ public class HomePage extends AppCompatActivity implements ChipNavigationBar.OnI
                 findViewById(R.id.profile_logout).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        // TODO : Add code for logout
                         Toast.makeText(HomePage.this, "Working!!!", Toast.LENGTH_SHORT).show();
                         slidingPane.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
                     }
